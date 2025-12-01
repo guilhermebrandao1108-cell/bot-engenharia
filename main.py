4a05899b5c1e04651a7d8ec4ae57307121753d92
Guilherme Brandão <guilhermebrandao1108@gmail.com>
2025-11-30 23:09:11 -0300

    Adiciona extracao de dados e inicio da compra

--- main.py
+++ main.py
@@ -1,11 +1,62 @@
+# Importar a classe principal do SeleniumBase
 from seleniumbase import SB
 
-# Esse comando abre o navegador, vai no Google e pesquisa
+# Iniciar o robô
 with SB() as sb:
-    sb.open("https://google.com")
-    sb.type('textarea[name="q"]', "Engenharia UFMG")
-    sb.click('input[value="Pesquisa Google"]')
+    # Guarda o endereço
+    url = "https://www.saucedemo.com/"
+    # Abre o site
+    sb.open(url)
+    # 1. Acesso ao Site 
+    #prenenche o nome do usuário
+    sb.type("#user-name", "standard_user")
+    #digita a senha
+    sb.type("#password", "secret_sauce")
+    #clica no botão de login
+    sb.click("#login-button")
+
     
-    # Espera 5 segundos para você ver o resultado
-    sb.sleep(5) 
-    print("Sucesso! O robô funcionou.")
+    # 2.Coleta de Dados 
+    #cria a lista com os nomes e preços dos produtos
+    nomes = sb.find_elements(".inventory_item_name")
+    precos = sb.find_elements(".inventory_item_price")
+    descricao = sb.find_elements(".inventory_item_desc")
+    
+    # Exibe a quantidade de produtos encontrados
+    print(f"{len(nomes)}")
+
+    #cria uma lista fazia para guardar o catálogo
+    catalogo = []
+
+    # O range cria um contador: 0, 1, 2, 3, 4, 5...
+    for i in range(len(nomes)):
+        # Pega o texto do nome número 'i'
+        texto_nome = nomes[i].text
+        # Pega o texto do preço número 'i'
+        texto_preco = precos[i].text
+        # Pega o textoda descrição número 'i
+        texto_descricao = descricao[i].text
+        # prepara o dicionário do produto atual
+        produto_atual = {"nome": texto_nome, "valor": texto_preco, "descricao": texto_descricao}
+        # Adiciona esse dicionário na lista principal
+        catalogo.append(produto_atual)
+    
+
+    # Exibe o catálogo completo
+    print("Dados salvos na memória:")
+    print(catalogo)    
+  
+
+    #3. Automação de Compra
+
+    #Adicionar Mochila ao carrinho
+    sb.click("#add-to-cart-sauce-labs-backpack")
+    # Ir para o carrinho de comptras
+    sb.click("shopping_cart_container")
+    # Iniciar o processo de checkout
+    sb.click("checkout")
+    
+    # Para conseguir ver o que apareceu  na tela, vamos esperar 8 segundos
+    sb.sleep(8)
+
+    
